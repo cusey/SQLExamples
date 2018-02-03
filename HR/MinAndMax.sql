974be097376d69b119ced42823bacfb2588d63e4
John Cusey <jjcusey@gmail.com>
2018-02-03 16:03:13 -0500

    Add files via upload

---
+++ HR/MinAndMax.sql
@@ -0,0 +1,9 @@
+/*
+The MIN() function returns the smallest value of the selected column.
+
+The MAX() function returns the largest value of the selected column.
+
+*/
+
+SELECT MIN(Salary) AS MinSalary FROM Employees;
+SELECT Max(Salary) AS MaxSalary FROM Employees;
