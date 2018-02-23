927067b21009c8b60ceff6ac9fee3a37d30a0637
John Cusey <jjcusey@gmail.com>
2018-02-23 17:34:31 +0000

    Update COMPUTERS.sql

--- HR/Schema/COMPUTERS.sql
+++ HR/Schema/COMPUTERS.sql
@@ -1,4 +1,31 @@
+Insert into BUSINESS.CUSTOMERS         
+  (SERIAL_NUMBER, MANUFACTURER, MODEL, EMPLOYEE_ID)
+Values    
+  ('SN00', 'HP;, 'G62-465DX', 'Henriot', 'Reims', 100);
 Insert into BUSINESS.CUSTOMERS       
   (SERIAL_NUMBER, MANUFACTURER, MODEL, EMPLOYEE_ID) 
 Values  
-  ('SN01', 'HP;, 'G62-465DX', 'Henriot', 'Reims', 100);
+  ('SN01', 'HP;, 'G62-465DX', 'Henriot', 'Reims', 101);
+Insert into BUSINESS.CUSTOMERS         
+  (SERIAL_NUMBER, MANUFACTURER, MODEL, EMPLOYEE_ID)
+Values   
+  ('SN02', 'HP;, 'G62-465DX', 'Henriot', 'Reims', 102);
+Insert into BUSINESS.CUSTOMERS           
+  (SERIAL_NUMBER, MANUFACTURER, MODEL, EMPLOYEE_ID)
+Values     
+  ('SN03', 'HP;, 'G62-465DX', 'Henriot', 'Reims', 103);
+Insert into BUSINESS.CUSTOMERS            
+  (SERIAL_NUMBER, MANUFACTURER, MODEL, EMPLOYEE_ID)
+Values      
+  ('SN04', 'HP;, 'G62-465DX', 'Henriot', 'Reims', 104);
+  
+
+   
+     
+   
+   
+   
+   
+   
+   
+   
