3f88d37c98d5aac3fafa1f331d1f5ccbeda7b27b
John Cusey <jjcusey@gmail.com>
2018-02-23 17:44:10 +0000

    Update COMPUTERS.sql

--- HR/Schema/COMPUTERS.sql
+++ HR/Schema/COMPUTERS.sql
@@ -1,23 +1,31 @@
-Insert into BUSINESS.CUSTOMERS         
+Insert into HP.COMPUTERS         
   (SERIAL_NUMBER, MANUFACTURER, MODEL, EMPLOYEE_ID)
 Values    
-  ('SN00', 'HP;, 'G62-465DX', 'Henriot', 'Reims', 100);
-Insert into BUSINESS.CUSTOMERS       
+  ('SN00', 'HP;, 'G62-465DX', 100);
+Insert into HP.COMPUTERS        
   (SERIAL_NUMBER, MANUFACTURER, MODEL, EMPLOYEE_ID) 
 Values  
-  ('SN01', 'HP;, 'G62-465DX', 'Henriot', 'Reims', 101);
-Insert into BUSINESS.CUSTOMERS         
+  ('SN01', 'HP;, 'G62-465DX', 101);
+Insert into HP.COMPUTERS          
   (SERIAL_NUMBER, MANUFACTURER, MODEL, EMPLOYEE_ID)
 Values   
-  ('SN02', 'HP;, 'G62-465DX', 'Henriot', 'Reims', 102);
-Insert into BUSINESS.CUSTOMERS           
+  ('SN02', 'HP;, 'G62-465DX', 102);
+Insert into HP.COMPUTERS            
   (SERIAL_NUMBER, MANUFACTURER, MODEL, EMPLOYEE_ID)
 Values     
-  ('SN03', 'HP;, 'G62-465DX', 'Henriot', 'Reims', 103);
-Insert into BUSINESS.CUSTOMERS            
+  ('SN03', 'HP;, 'G62-465DX', 103);
+Insert into HP.COMPUTERS             
   (SERIAL_NUMBER, MANUFACTURER, MODEL, EMPLOYEE_ID)
 Values      
-  ('SN04', 'HP;, 'G62-465DX', 'Henriot', 'Reims', 104);
+  ('SN04', 'HP;, 'G62-465DX', 104);
+Insert into HP.COMPUTERS              
+   (SERIAL_NUMBER, MANUFACTURER, MODEL, EMPLOYEE_ID)
+ Values        
+   ('SN05', 'HP;, 'G62-465DX',  null);
+Insert into HP.COMPUTERS                 
+   (SERIAL_NUMBER, MANUFACTURER, MODEL, EMPLOYEE_ID) 
+ Values          
+   ('SN06', 'HP;, 'G62-465DX',  null);
   
 
    
