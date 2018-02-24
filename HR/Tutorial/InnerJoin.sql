5eb9fe0df34ca0836124ce90ec0abddabb2f2223
John Cusey <jjcusey@gmail.com>
2018-02-24 15:14:35 -0500

    Update InnerJoin.sql

--- HR/Tutorial/InnerJoin.sql
+++ HR/Tutorial/InnerJoin.sql
@@ -7,3 +7,14 @@ determine who needs to sign a Computer Users Policy Agreement.
 SELECT E.EMPLOYEE_ID, E.LAST_NAME, C.SERIAL_NUMBER
 FROM EMPLOYEES E INNER JOIN  COMPUTERS C 
 ON E.EMPLOYEE_ID = C.EMPLOYEE_ID; 
+
+--OUTPUT
+/*
+EMPLOYEE_ID LAST_NAME                 SERIA
+----------- ------------------------- -----
+        102 De Haan                   SN02 
+        104 Ernst                     SN04 
+        103 Hunold                    SN03 
+        100 King                      SN00 
+        101 Kochhar                   SN01 
+*/
